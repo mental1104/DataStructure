b4feaa730ad4270f7d717ef4f67c6e93e212bfcb
mental1104 <mental1104@gmail.com>
2023-05-13 23:55:44 +0800

    CMake优化

--- src/include/pq/Heap.h
+++ src/include/pq/Heap.h
@@ -1,10 +1,18 @@
+/*
+ * @Author: mental1104 mental1104@gmail.com
+ * @Date: 2023-05-06 19:43:26
+ * @LastEditors: mental1104 mental1104@gmail.com
+ * @LastEditTime: 2023-05-13 23:28:50
+ * @FilePath: /espeon/code/DataStructure/src/include/pq/Heap.h
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 #pragma once 
 
 #include "PQ.h"  
 #include "Vector.h"
 
 template <typename T> 
-class Heap : public PQ<T>, public Vector<T> { //完全二叉堆
+class Heap : public PQ<T>, public Vector<T> { //完全二叉堆include_directories(${TOP_DIR}/src/include)
    /*DSA*/friend class UniPrint; //演示输出使用，否则不必设置友类
 private:
     Rank Parent(Rank i){    return (i - 1)/2;   }
