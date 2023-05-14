834ade4281fce36e222cca04290e6df5fc14997a
mental1104 <mental1104@gmail.com>
2023-05-14 22:10:31 +0800

    测试for_each Vector

--- src/vector/Vector.cpp
+++ src/vector/Vector.cpp
@@ -3,11 +3,13 @@
 #include "Vector.h"
 #include "print.h" 
 
-
 int main(){
     Vector<int> vec;
-    for(int i = 0; i < 10; i++)
+    for(int i = 0; i < 100; i++)
         vec.insert(i);
     print(vec);
+    for(auto i : vec)
+        std::cout << i << " ";
+    std::cout << std::endl;
     return 0;
 }
