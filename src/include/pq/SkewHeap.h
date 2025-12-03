e5a659a26fcd9ffa783f3e70ab1273b73d050919
mental1104 <mental1104@gmail.com>
2025-12-03 23:11:11 +0800

    [ADD] 引入斜堆

---
+++ src/include/pq/SkewHeap.h
@@ -0,0 +1,78 @@
+#ifndef __DSA_SKEW_HEAP
+#define __DSA_SKEW_HEAP
+
+#include <stdexcept>
+#include "BinTree.h"
+#include "PQ.h"
+#include "Vector.h"
+
+template <typename T>
+class SkewHeap : public PQ<T>, public BinTree<T> { //基于二叉树，以斜堆形式实现的PQ
+   /*DSA*/friend class UniPrint; //演示输出使用，否则不必设置友类
+private:
+    BinNode<T>* merge(BinNode<T>* a, BinNode<T>* b);
+public:
+    SkewHeap() { } //默认构造
+    SkewHeap ( T* E, int n ) //批量构造
+    {  for ( int i = 0; i < n; i++ ) insert ( E[i] );  }
+    SkewHeap(Vector<T>& vec){
+        for (int i = 0; i < vec.size(); i++)
+            insert(vec[i]);
+    }
+   
+    void merge(SkewHeap<T>& right);
+    void insert(T); //按照比较器确定的优先级次序插入元素
+    T getMax(); //取出优先级最高的元素
+    T delMax(); //删除优先级最高的元素
+}; //SkewHeap
+
+template <typename T> //合并以a和b为根节点的两个斜堆
+BinNode<T>* SkewHeap<T>::merge(BinNode<T>* a, BinNode<T>* b) {
+    if ( !a ) return b;
+    if ( !b ) return a;
+    if ( a->data < b->data )
+        swap ( a, b ); //一般情况：首先确保b不大
+
+    a->rc = merge(a->rc, b); //将a的右子堆，与b合并
+    if (a->rc) a->rc->parent = a;
+
+    swap (a->lc, a->rc); //斜堆特性：每次合并后交换左右子堆
+    return a; //返回合并后的堆顶
+} //本算法只实现结构上的合并，堆的规模须由上层调用者负责更新
+
+template <typename T> 
+void SkewHeap<T>::insert (T e){
+   this->_root = merge(this->_root, new BinNode<T>(e, nullptr)); //将e封装为斜堆，与当前斜堆合并
+   this->_size++; //更新规模
+}
+
+template <typename T> 
+T SkewHeap<T>::getMax(){
+    if (!this->_root) 
+        throw std::runtime_error("Heap is empty");
+    return this->_root->data; 
+} //按照此处约定，堆顶即优先级最高的词条
+
+template <typename T> 
+T SkewHeap<T>::delMax() {
+   if (!this->_root) 
+      throw std::runtime_error("Heap is empty");
+   BinNode<T>* lHeap = this->_root->lc; if (lHeap) lHeap->parent = NULL; //左子堆
+   BinNode<T>* rHeap = this->_root->rc; if (rHeap) rHeap->parent = NULL; //右子堆
+   T e = this->_root->data; 
+   delete this->_root; 
+   this->_size--; //删除根节点
+   this->_root = merge ( lHeap, rHeap ); //合并原左、右子堆
+   return e; //返回原根节点的数据项
+}
+
+template<typename T>
+void SkewHeap<T>::merge(SkewHeap<T>& right){
+    this->_root = merge(this->_root, right._root);
+    right._root = nullptr;
+    this->_size += right._size;
+    right._size = 0;
+    return;
+}
+
+#endif
---
+++ test/test_skew_heap.cpp
@@ -0,0 +1,45 @@
+#include <gtest/gtest.h>
+#include <stdexcept>
+#include "SkewHeap.h"
+
+// 插入元素
+TEST(SkewHeapTest, InsertTest) {
+    SkewHeap<int> heap;
+    heap.insert(10);
+    heap.insert(20);
+    heap.insert(5);
+    EXPECT_EQ(heap.size(), 3);
+    EXPECT_EQ(heap.getMax(), 20);
+}
+
+// 删除最大值
+TEST(SkewHeapTest, DeleteMaxTest) {
+    SkewHeap<int> heap;
+    heap.insert(10);
+    heap.insert(20);
+    heap.insert(5);
+    EXPECT_EQ(heap.delMax(), 20);
+    EXPECT_EQ(heap.size(), 2);
+    EXPECT_EQ(heap.getMax(), 10);
+}
+
+// 合并两个斜堆
+TEST(SkewHeapTest, MergeTest) {
+    SkewHeap<int> heap1, heap2;
+    heap1.insert(10);
+    heap1.insert(30);
+    heap2.insert(20);
+    heap2.insert(40);
+
+    heap1.merge(heap2);
+    EXPECT_EQ(heap1.getMax(), 40);
+    EXPECT_EQ(heap1.size(), 4);
+    EXPECT_TRUE(heap2.empty());
+}
+
+// 空堆操作
+TEST(SkewHeapTest, EmptyHeapTest) {
+    SkewHeap<int> heap;
+    EXPECT_THROW(heap.getMax(), std::runtime_error);
+    EXPECT_THROW(heap.delMax(), std::runtime_error);
+}
