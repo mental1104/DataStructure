1eb84773131429009f839fe9671769b36219b871
Blue Espeon <mental1104@gmail.com>
2026-07-21 00:55:50 +0800

    refactor: 完善堆优先级策略接口 [skip ci]

--- src/include/pq/PQ.h
+++ src/include/pq/PQ.h
@@ -11,15 +11,24 @@
 #endif
 #endif
 
+// 教学优先级策略：MAX=true 时较大值优先，否则较小值优先。
 template<typename T, bool MAX = true>
 struct Priority {
-    static bool higher(const T& a, const T& b) { //a 是否比 b 优先
-        return MAX ? b < a : a < b; //仅依赖operator<，减少自定义类型要求
+    // 判断 a 的优先级是否高于 b，仅要求 T 提供 operator<。
+    static bool higher(const T& a, const T& b) {
+        return MAX ? b < a : a < b;
+    }
+
+    // 作为共享堆算法的函数对象入口。
+    bool operator()(const T& a, const T& b) const {
+        return higher(a, b);
     }
 };
 
+// 教学优先队列抽象接口；具体结构继续保留 insert/getMax/delMax 历史命名。
 template<typename T, bool MAX = true>
 struct PQ {
+    virtual ~PQ() {}
     virtual void insert(T) = 0;
     virtual T getMax() = 0;
     virtual T delMax() = 0;
