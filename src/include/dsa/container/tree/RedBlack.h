646da00dd98430d17db8d108829648dd7c87f78b
Blue Espeon <mental1104@gmail.com>
2026-07-21 00:12:16 +0800

    refactor: add allocator-aware red-black tree container

---
+++ src/include/dsa/container/tree/RedBlack.h
@@ -0,0 +1,19 @@
+#ifndef DSA_CONTAINER_TREE_RED_BLACK_H
+#define DSA_CONTAINER_TREE_RED_BLACK_H
+
+#include <dsa/container/tree/BST.h>
+
+namespace dsa {
+namespace container {
+
+template<
+    typename T,
+    typename Compare = std::less<T>,
+    typename Allocator = std::allocator<T>
+>
+using RedBlack = detail::BasicSearchTree<T, Compare, Allocator, detail::RedBlackTreeTag>;
+
+} // namespace container
+} // namespace dsa
+
+#endif
